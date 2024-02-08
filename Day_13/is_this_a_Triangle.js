75343bb92fcc529eaae62cac8887680b71b018c2
Hassan Boukatena <hassanboukatena@gmail.com>
2024-02-08 11:27:14 +0100

    code wars

---
+++ Day_13/is_this_a_Triangle.js
@@ -0,0 +1,11 @@
+function triangle(a,b,c){
+
+   if(a + b > c & a + c > b & b + c > a){
+       console.log("the triangle is  valide ");
+    
+}else{
+    console.log("the triangle is not valide ");
+   
+}
+}
+triangle(1,2,2);
