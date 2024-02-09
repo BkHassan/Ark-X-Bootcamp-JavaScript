2d322f272ba354edd4ae97693b5cf1d7ff9e0dc8
Hassan Boukatena <hassanboukatena@gmail.com>
2024-02-09 10:55:57 +0100

    temperature

---
+++ Day_13/temperature.js
@@ -0,0 +1,32 @@
+const cities = [
+  { name: "New York", lat: 40.7128, lng: -74.006 },
+  { name: "London", lat: 51.5074, lng: -0.1278 },
+  { name: "Paris", lat: 48.8566, lng: 2.3522 },
+  { name: "Tokyo", lat: 35.6895, lng: 139.6917 },
+  { name: "Sydney", lat: -33.8651, lng: 151.2099 },
+  { name: "Rome", lat: 41.9028, lng: 12.4964 },
+  { name: "Cairo", lat: 30.0444, lng: 31.2357 },
+  { name: "Rio de Janeiro", lat: -22.9068, lng: -43.1729 },
+  { name: "Dubai", lat: 25.2048, lng: 55.2708 },
+  { name: "Rabat", lat: 34.0209, lng: -6.8416 },
+];
+
+function selectRandomCity(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  const randomCity = arr[randomIndex];
+  return randomCity;
+}
+let RandomCity = selectRandomCity(cities);
+console.log(RandomCity.name);
+
+let lat = RandomCity.lat;
+let lng = RandomCity.lng;
+
+async function fetching() {
+  const response = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current_weather=true`
+  );
+  const data = await response.json();
+  console.log(data.current_weather.temperature + " " + data.current_weather_units.temperature);
+}
+fetching()
